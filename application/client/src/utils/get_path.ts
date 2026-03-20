a2dd915887348529f9e8a0425cd56c041154b6a7
Keiji Miyazaki <miyah.dev@gmail.com>
2026-03-20 16:53:51 +0900

    静的ファイルがCDNでキャッシュされてしまったため、キャッシュを飛ばすパラメーターを追加

--- application/client/src/utils/get_path.ts
+++ application/client/src/utils/get_path.ts
@@ -1,15 +1,17 @@
+const CACHE_BUSTER = "v=2";
+
 export function getImagePath(imageId: string): string {
-  return `/images/${imageId}.webp`;
+  return `/images/${imageId}.webp?${CACHE_BUSTER}`;
 }
 
 export function getMoviePath(movieId: string): string {
-  return `/movies/${movieId}.gif`;
+  return `/movies/${movieId}.gif?${CACHE_BUSTER}`;
 }
 
 export function getSoundPath(soundId: string): string {
-  return `/sounds/${soundId}.mp3`;
+  return `/sounds/${soundId}.mp3?${CACHE_BUSTER}`;
 }
 
 export function getProfileImagePath(profileImageId: string): string {
-  return `/images/profiles/${profileImageId}.webp`;
+  return `/images/profiles/${profileImageId}.webp?${CACHE_BUSTER}`;
 }
