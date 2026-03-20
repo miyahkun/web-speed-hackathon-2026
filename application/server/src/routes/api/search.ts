86c5638e148cc8c939f794d28ecfd59badcab83c
Keiji Miyazaki <miyah.dev@gmail.com>
2026-03-20 18:09:08 +0900

    SQLite のクエリ実行エラーを修正

--- application/server/src/routes/api/search.ts
+++ application/server/src/routes/api/search.ts
@@ -55,10 +55,10 @@ searchRouter.get("/search", async (req, res) => {
   let postsByUser: typeof postsByText = [];
   if (searchTerm) {
     postsByUser = await Post.findAll({
+      subQuery: false,
       include: [
         {
           association: "user",
-          attributes: { exclude: ["profileImageId"] },
           include: [{ association: "profileImage" }],
           required: true,
           where: {
